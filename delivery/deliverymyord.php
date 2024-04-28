0e68da55d2c4edb9bb2861ff711f12e139335673
GSOWMY <143527959+GSOWMY@users.noreply.github.com>
2024-04-28 19:10:23 +0530

    Add files via upload

--- delivery/deliverymyord.php
+++ delivery/deliverymyord.php
@@ -1,26 +1,15 @@
 <?php
 ob_start(); 
+
 // $connection = mysqli_connect("localhost:3307", "root", "");
 // $db = mysqli_select_db($connection, 'demo');
-include("connect.php"); 
 include '../connection.php';
+ include("connect.php"); 
 if($_SESSION['name']==''){
 	header("location:deliverylogin.php");
 }
 $name=$_SESSION['name'];
-$city=$_SESSION['city'];
-$ch=curl_init();
-curl_setopt($ch,CURLOPT_URL,"http://ip-api.com/json");
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-$result=curl_exec($ch);
-$result=json_decode($result);
-// $city= $result->city;
-// echo $city;
-
 $id=$_SESSION['Did'];
-
-
-
 ?>
 
 
@@ -31,10 +20,8 @@ $id=$_SESSION['Did'];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script language="JavaScript" src="http://www.geoplugin.net/javascript.gp" type="text/javascript"></script>
-    <link rel="stylesheet" href="../home.css">
-
     <link rel="stylesheet" href="delivery.css">
+    <link rel="stylesheet" href="../home.css">
 </head>
 <body>
 <header>
@@ -46,10 +33,10 @@ $id=$_SESSION['Did'];
         </div>
         <nav class="nav-bar">
             <ul>
-                <li><a href="#home" class="active">Home</a></li>
+                <li><a href="delivery.php" >Home</a></li>
                 <li><a href="openmap.php" >map</a></li>
-                <li><a href="deliverymyord.php" >myorders</a></li>
-                <!-- <li ><a href="../logout.php"  >Logout</a></li> -->
+                <li><a href="deliverymyord.php"  class="active">myorders</a></li>
+    
             </ul>
         </nav>
     </header>
@@ -61,14 +48,6 @@ $id=$_SESSION['Did'];
             navBar.classList.toggle("active");
         }
     </script>
-<?php
-
-// echo var_export(unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip=103.113.190.19')));
-// echo "Your city: {$city}\n";
-
-// $city = "<script language=javascript> document.write(geoplugin_city());</script>"; 
-// $scity=$city;
-?>
     <style>
         .itm{
             background-color: white;
@@ -81,7 +60,7 @@ $id=$_SESSION['Did'];
             margin-right: auto;
         }
         p{
-            text-align: center; font-size: 30PX;color: black; margin-top: 50px;
+            text-align: center; font-size: 28PX;color: black; 
         }
         a{
             /* text-decoration: underline; */
@@ -97,23 +76,22 @@ $id=$_SESSION['Did'];
             }
         }
     </style>
-         <h2><center>Welcome <?php echo"$name";?></center></h2>
 
         <div class="itm" >
 
             <img src="../img/delivery.gif" alt="" width="400" height="400"> 
           
         </div>
-        <!-- <h2><center>your Location : <?php echo"$city" ?></center></h2> -->
+
         <div class="get">
             <?php
 
 
 // Define the SQL query to fetch unassigned orders
-$sql = "SELECT fd.Fid AS Fid,fd.location as cure, fd.name,fd.phoneno,fd.date,fd.delivery_by, fd.address as From_address, 
+$sql = "SELECT fd.Fid AS Fid, fd.name,fd.phoneno,fd.date,fd.delivery_by, fd.address as From_address, 
 ad.name AS delivery_person_name, ad.address AS To_address
 FROM food_donations fd
-LEFT JOIN admin ad ON fd.assigned_to = ad.Aid where assigned_to IS NOT NULL and   delivery_by IS NULL and fd.location='$city';
+LEFT JOIN admin ad ON fd.assigned_to = ad.Aid where delivery_by='$id';
 ";
 
 // Execute the query
@@ -136,14 +114,6 @@ while ($row = mysqli_fetch_assoc($result)) {
 if (isset($_POST['food']) && isset($_POST['delivery_person_id'])) {
     $order_id = $_POST['order_id'];
     $delivery_person_id = $_POST['delivery_person_id'];
-    $sql = "SELECT * FROM food_donations WHERE Fid = $order_id AND delivery_by IS NOT NULL";
-    $result = mysqli_query($connection, $sql);
-
-    if (mysqli_num_rows($result) > 0) {
-        // Order has already been assigned to someone else
-        die("Sorry, this order has already been assigned to someone else.");
-    }
-
 
     $sql = "UPDATE food_donations SET delivery_by = $delivery_person_id WHERE Fid = $order_id";
     // $result = mysqli_query($conn, $sql);
@@ -165,8 +135,9 @@ if (isset($_POST['food']) && isset($_POST['delivery_person_id'])) {
 ?>
 <div class="log">
 <!-- <button type="submit" name="food" onclick="">My orders</button> -->
-<a href="deliverymyord.php">My orders</a>
-
+<a href="delivery.php">Take orders</a>
+<p>Order assigned to you</p>
+<br>
 </div>
   
 
@@ -183,8 +154,8 @@ if (isset($_POST['food']) && isset($_POST['delivery_person_id'])) {
             <th>phoneno</th>
             <th>date/time</th>
             <th>Pickup address</th>
-            <th>Delivery Address</th>
-            <th>Action</th>
+            <th>Delivery address</th>
+            <!-- <th>Orders</th> -->
          
           
            
@@ -199,18 +170,12 @@ if (isset($_POST['food']) && isset($_POST['delivery_person_id'])) {
             <!-- <td><?= $row['Fid'] ?></td>
             <td><?= $row['name'] ?></td>
             <td><?= $row['address'] ?></td> -->
-            <td data-label="Action" style="margin:auto">
-                <?php if ($row['delivery_by'] == null) { ?>
-                    <form method="post" action=" ">
-                        <input type="hidden" name="order_id" value="<?= $row['Fid'] ?>">
-                        <input type="hidden" name="delivery_person_id" value="<?= $id ?>">
-                        <button type="submit" name="food">Take order</button>
-                    </form>
-                <?php } else if ($row['delivery_by'] == $id) { ?>
+            <!-- <td data-label="Action" style="margin:auto"> -->
+                <!-- <?php if ($row['delivery_by'] == $id) { ?>
                     Order assigned to you
                 <?php } else { ?>
                     Order assigned to another delivery person
-                <?php } ?>
+                <?php } ?> -->
             </td>
         </tr>
         <?php } ?>
